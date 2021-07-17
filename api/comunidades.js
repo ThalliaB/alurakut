7d407156e565f80288a11a73101553daad6f5b35
ThalliaB <79876271+ThalliaB@users.noreply.github.com>
2021-07-17 13:58:39 -0300

    Create login pagen in the project

---
+++ api/comunidades.js
@@ -0,0 +1,29 @@
+import { SiteClient } from 'datocms-client';
+
+export default async function recebedorDeRequests(request, response) {
+    if(request.method === 'POST') {
+        const TOKEN = 'b34e916a0d92a52c6216ed231eb14c';
+        const client = new SiteClient(TOKEN);
+
+        // Validar os dados, antes de sair cadastrando
+        const registroCriado = await client.items.create({
+            itemType: "970988", // ID do Model de "Communities" criado pelo Dato
+            ...request.body,
+            // title: "Comunidade de Teste",
+            // imageUrl: "https://github.com/omariosouto.png",
+            // creatorSlug: "omariosouto"
+        })
+
+        console.log(registroCriado);
+
+        response.json({
+            dados: 'Algum dado qualquer',
+            registroCriado: registroCriado,
+        })
+        return;
+    }
+
+    response.status(404).json({
+        message: 'Ainda não temos nada no GET, mas no POST tem!'
+    })
+} 
